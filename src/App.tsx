bfbe0e41a3ea0dc8737cfdad3285ee7496be5b21
Karasaev Magomed <rostovmen95@gmail.com>
2026-03-16 20:32:52 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.scss'; // Импортируем без переменной
-import Title from '@/components/Title.tsx'
+import spiderImg from '@/assets/images/spider.png';
+
+
 function App() {
   return (
     // Используем просто строку с названием класса
     <div className="container"> 
-      <Title/>
       <p>Webpack теперь собирает и это.</p>
     </div>
   );
